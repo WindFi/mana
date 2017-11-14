7ae2f265e49486900fbf2725459ac527522f52b2
WindFi <wuminshren@gmail.com>
2017-11-14 15:51:08 +0800

    add ormlite

--- app/src/main/java/me/sunzheng/mana/home/mybangumi/FavoritesFragment.java
+++ app/src/main/java/me/sunzheng/mana/home/mybangumi/FavoritesFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -92,9 +93,10 @@ public class FavoritesFragment extends Fragment implements HomeContract.MyBangum
     public void setAdapter(RecyclerView.Adapter adapter) {
         if (mRecyclerView == null)
             return;
-        if (mRecyclerView.getAdapter() == null)
+        if (mRecyclerView.getAdapter() == null) {
             mRecyclerView.setAdapter(adapter);
-        else
+            mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        } else
             mRecyclerView.swapAdapter(adapter, true);
         if (mRecyclerView.getLayoutManager() == null)
             mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
