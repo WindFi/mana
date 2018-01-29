8e5b4a739d7fadd1052fd4e5f20cdfb3dd94e267
WindFi <wuminshren@gmail.com>
2018-01-29 11:45:35 +0800

    downgrade support lib version

--- app/src/main/java/me/sunzheng/mana/home/onair/OnAirFragment.java
+++ app/src/main/java/me/sunzheng/mana/home/onair/OnAirFragment.java
@@ -57,8 +57,8 @@ public class OnAirFragment extends Fragment implements HomeContract.OnAir.View {
     public android.view.View onCreateView(LayoutInflater inflater, ViewGroup container,
                                           Bundle savedInstanceState) {
         android.view.View view = inflater.inflate(R.layout.fragment_item_list, container, false);
-        recyclerView = view.findViewById(R.id.list);
-        swipeRefreshLayout = view.findViewById(R.id.swiperefreshlayout);
+        recyclerView = (RecyclerView) view.findViewById(R.id.list);
+        swipeRefreshLayout = (SwipeRefreshLayout) view.findViewById(R.id.swiperefreshlayout);
         return view;
     }
 
